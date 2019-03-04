4c1a99673aaf78ad064828981e3379d0ac1b6878
vodaza36 <thomas@hochbichler.at>
2019-03-04 16:06:34 +0100

    adopt index js

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const client = require('./service')
-
-// returns the water consumption of the last 14-days
-const history = client.getWaterConsumptionHistory()
-  .then(result => console.log('History: ', result))
-
-// returns yesterdays water consumption
-const yesterday = client.getWaterConsumptionYesterday()
-  .then(result => console.log('Yesterday: ', result))
+require('./service')
