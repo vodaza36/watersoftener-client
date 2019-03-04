1ecde86aaba7a9eb0f1eb9227e0894d64f36fd6c
vodaza36 <thomas@hochbichler.at>
2019-03-04 16:28:47 +0100

    add env check

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('./service')
+const service = require('./service')
+
+if (!process.env.WATERSOFTENER_IP) {
+  console.log('Environment variable WATERSOFTENER_IP not defined!')
+  process.exit(-1)
+}
+
+module.exports = service
